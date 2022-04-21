afaa21d3e6e99668e807c9f61f8af3588ee171f0
Zwerry <56406513+Zwerry@users.noreply.github.com>
2022-04-21 23:22:13 +0900

    initial commit

--- KatjasConsoleapplication/KatjasConsoleapplication/Program.cs
+++ KatjasConsoleapplication/KatjasConsoleapplication/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, wetrfawertawert!");
 Console.WriteLine("Hello, wetrfawertawert!");
+Console.WriteLine("Hello, wetrfawertawert!");
+Console.WriteLine("Hello, wetrfawertawert!");
